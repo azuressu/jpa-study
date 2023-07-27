8b0a0ca4cd9e7339e5c778154df1275a20f02b5b
azuressu <martina0130@naver.com>
2023-07-27 19:15:11 +0900

    @EmbeddedId로 복합키 설정

--- src/main/java/me/whitebear/jpastudy/userChannel/UserChannel.java
+++ src/main/java/me/whitebear/jpastudy/userChannel/UserChannel.java
@@ -14,13 +14,15 @@ import me.whitebear.jpastudy.user.User;
 
 //jpa
 @Entity
-@IdClass(UserChannelId.class)
 public class UserChannel {
 
 //    @Id
 //    @GeneratedValue(strategy = GenerationType.IDENTITY)
 //    @Column(name = "id", nullable = false)
 //    private Long id;
+
+    @EmbeddedId
+    private UserChannelId userChannelId;
 
     /**
      * 컬럼 - 연관관계 컬럼을 제외한 컬럼을 정의합니다.
@@ -39,14 +41,12 @@ public class UserChannel {
     /**
      * 연관관계 - Foreign Key 값을 따로 컬럼으로 정의하지 않고 연관 관계로 정의합니다.
      */
-    @Id
     @ManyToOne
-    @JoinColumn(name = "user_id")
+    @MapsId("user_id")
     User user;
 
-    @Id
     @ManyToOne
-    @JoinColumn(name = "channel_id")
+    @MapsId("channel_id")
     Channel channel;
 
     /**
--- src/main/java/me/whitebear/jpastudy/userChannel/UserChannelId.java
+++ src/main/java/me/whitebear/jpastudy/userChannel/UserChannelId.java
@@ -1,29 +1,40 @@
 package me.whitebear.jpastudy.userChannel;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Embeddable
 public class UserChannelId implements Serializable {
-    private Long user;   // UserChannel의 user 필드명과 동일해야 함
-    private Long channel; // UserChannel의 channel 필드명과 동일해야 함
+
+    @Serial
+    private static final long serialVersionUID = 98234598273491834L;
+
+    @Column(name = "user_id")
+    private Long userId;   // UserChannel의 user 필드명과 동일해야 함
+
+    @Column(name = "channel_id")
+    private Long channelId; // UserChannel의 channel 필드명과 동일해야 함
 
     @Override
-    public boolean equals(Object o){
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+    public boolean equals(Object o) {
+        if(this == o) return true;
+        if(o==null || getClass() != o.getClass()) return false;
         UserChannelId userChannelId = (UserChannelId) o;
-
-        return Objects.equals(getUser(), userChannelId.getUser()) && Objects.equals(getChannel(), userChannelId.getChannel());
+        return Objects.equals(getUserId(), userChannelId.getUserId()) && Objects.equals(getChannelId(), userChannelId.getChannelId());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getUser(), getChannel());
+        return Objects.hash(getUserId(), getChannelId());
     }
+
 }
