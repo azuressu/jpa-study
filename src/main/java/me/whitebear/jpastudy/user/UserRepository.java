707c318cd6be68ea7b96743cc9039091f3307933
azuressu <martina0130@naver.com>
2023-07-28 12:15:47 +0900

    @RepositoryDefinition 사용

--- src/main/java/me/whitebear/jpastudy/user/UserRepository.java
+++ src/main/java/me/whitebear/jpastudy/user/UserRepository.java
@@ -1,6 +1,12 @@
 package me.whitebear.jpastudy.user;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.RepositoryDefinition;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+import java.util.Optional;
+
+@RepositoryDefinition(domainClass = User.class, idClass = Long.class)
+public interface UserRepository{
+    public Optional<User> findByUsername(String username);
+
+    // findByPassword를 막기 위해 (username만 갖고 조회할 수 있도록 함)
 }
